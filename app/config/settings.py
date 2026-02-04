4aed5c2d8a4e18bab4c6572dc7310b2a8f7b649d
Ubuntu <ubuntu@ip-172-31-21-234.us-east-2.compute.internal>
2026-02-04 18:06:46 +0000

    updated allowed hosts

--- app/config/settings.py
+++ app/config/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', 'dev-secret')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DJANGO_DEBUG", "0") == "1"
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1", "0.0.0.0", '3.143.238.49']
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "0.0.0.0", '18.117.129.194']
 
 
 # Application definition
