702d11d3b51e097141ed0e46fddf7ba30061d3ee
biplovPoudel <biplovpoudel18@gmail.com>
2026-03-01 14:10:06 -0600

    changed the urlpattern to redirect to the custom admin site

--- app/config/urls.py
+++ app/config/urls.py
@@ -1,7 +1,7 @@
-from django.contrib import admin
 from django.urls import path, include
+from items.admin import gradesync_admin
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    path("admin/", gradesync_admin.urls),
     path("", include("items.urls")),
 ]
