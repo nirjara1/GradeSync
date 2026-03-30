34a420ee262eb6409f2fdf5e3e640ca3e7c0fec3
Ishant Shrestha <Shresthai@warhawks.ulm.edu>
2026-03-29 20:40:53 -0500

    feat: implement add student search and autocomplete in the add student modal

--- backend/professor/urls.py
+++ backend/professor/urls.py
@@ -18,6 +18,7 @@ urlpatterns = [
     
     # API endpoints
     path('api/courses/<int:course_id>/students/', api.add_students_api, name='api_add_students'),
+    path('api/courses/<int:course_id>/search-students/', api.search_students_api, name='api_search_students'),
     path('api/courses/<int:course_id>/graders/', api.add_graders_api, name='api_add_graders'),
     path('api/courses/<int:course_id>/roster/', api.course_roster_api, name='api_course_roster'),
     path('api/courses/<int:course_id>/remove/', api.remove_member_api, name='api_remove_member'),
